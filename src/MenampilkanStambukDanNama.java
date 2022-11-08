12e1da0799364ad8bba9b18145982cd7435ea876
ASUS <ASUS@LAPTOP-F4UON8SK>
2022-11-08 19:36:09 +0800

    First Commit

---
+++ src/MenampilkanStambukDanNama.java
@@ -0,0 +1,14 @@
+public class MenampilkanStambukDanNama {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //
+        int Stambuk,Nama;
+        //
+        System.out.println("Stambuk = 2261007");
+        System.out.println("Nama = Arya Setiawan Tanda");
+        
+    }
+    
+}
